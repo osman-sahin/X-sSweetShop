9f6a35ac315080fd4d6a14e814063c17dfb7d273
osman-sahin <osmansahin.eng@gmail.com>
2020-05-19 19:50:59 +0300

    Startup scope redirected to db repositories, db created and seeded

---
+++ X-sSweetShop/Migrations/20200519164533_NotesAddedonPie.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace X_sSweetShop.Migrations
+{
+    public partial class NotesAddedonPie : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Notes",
+                table: "Pies",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Notes",
+                table: "Pies");
+        }
+    }
+}
