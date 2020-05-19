0937ec53713a77b96ff4d8f2d668b1a117699f69
osman-sahin <osmansahin.eng@gmail.com>
2020-05-19 03:24:19 +0300

    Add project files.

---
+++ X-sSweetShop/ViewModels/PiesListViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using X_sSweetShop.Models;
+
+namespace X_sSweetShop.ViewModels
+{
+    public class PiesListViewModel
+    {
+        public IEnumerable<Pie> Pies { get; set; }
+        public string CurrentCategory { get; set; }
+    }
+}
